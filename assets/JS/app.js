390d0d439f7e4dbf423204809132e1077483b731
fbuitron <franciscobuitron21@gmail.com>
2020-02-05 09:47:51 -0800

    changed to https

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,13 +1,13 @@
 document.getElementById('submit').addEventListener('click', event => {
   event.preventDefault()
   let userSearch = document.getElementById('input_text').value
-  fetch(`http://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSearch}&APPID=74d732ec0d08c3c84269e1425799d474`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSearch}&APPID=74d732ec0d08c3c84269e1425799d474`)
     .then(r => r.json())
     .then(weather => {
       console.log(weather)
       let sec = `${weather.dt}`
       let date = new Date(sec*1000)
-      document.getElementById('city-name').innerHTML = `${weather.name}, ${date} <img class="icon" src="http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png"></img>`
+      document.getElementById('city-name').innerHTML = `${weather.name}, ${date} <img class="icon" src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png"></img>`
       document.getElementById('city-info').innerHTML = `
       Temperature: ${weather.main.temp} ℉
       <br>

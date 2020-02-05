40f78500b4ba863dc5fb43ea0028783842d262f3
fbuitron <franciscobuitron21@gmail.com>
2020-02-05 14:59:50 -0800

    added card deck, title changes with date

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -22,4 +22,23 @@ document.getElementById('submit').addEventListener('click', event => {
   city.textContent = userSearch
   document.getElementById('list').append(city)
   document.getElementById('input_text').value = ''
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=${userSearch}&APPID=74d732ec0d08c3c84269e1425799d474`)
+    .then(r => r.json())
+    .then(forecast => {
+      console.log(forecast)
+      document.getElementById('date1').textContent = `${forecast.list[0].dt_txt}`
+      document.getElementById('date2').textContent = `${forecast.list[8].dt_txt}`
+      document.getElementById('date3').textContent = `${forecast.list[16].dt_txt}`
+      document.getElementById('date4').textContent = `${forecast.list[24].dt_txt}`
+      document.getElementById('date5').textContent = `${forecast.list[32].dt_txt}`
+      // document.getElementById('city-name').innerHTML = `${weather.name}, ${date} <img class="icon" src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png"></img>`
+      // document.getElementById('city-info').innerHTML = `
+      // Temperature: ${weather.main.temp} ℉
+      // <br>
+      // Humidity: ${weather.main.humidity}%
+      // <br>
+      // Wind Speed: ${weather.wind.speed} MPH
+      // `
+    })
+    .catch(e => console.error(e))
 })
